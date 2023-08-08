5f66e74ba71d8b1fa28242a0c13111f182f55274
Jhonatan <jhonjhonpf@hotmail.com>
2023-08-08 20:41:32 -0300

    Atualizacao

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+//componentes
+import Home from './pages/Home/'
+
+//estilizacao global
+import "./index.css";
+import ListaServicos from './pages/ListaServicos';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <Home /> */}
+    <ListaServicos />
   </React.StrictMode>,
 )
