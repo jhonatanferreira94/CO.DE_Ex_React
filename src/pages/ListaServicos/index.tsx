dda01f0b6454eebc24ecae0e3ea1a3b4ed949703
Jhonatan <jhonjhonpf@hotmail.com>
2023-08-17 21:50:22 -0300

    atualizacao

--- src/pages/ListaServicos/index.tsx
+++ src/pages/ListaServicos/index.tsx
@@ -5,99 +5,117 @@ import "./style.css";
 import CardServico from "../../components/CardServico";
 
 //hoks
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+//api
+import api from "../../utils/api";
 
 
 function ListaServicos() {
 
-    //STATE DEVS
+  //STATE DEVS
 
-    const [servicos, setServicos] = useState <any[]>([
-        {
-          titulo: "Desenvolvimento de site institucional - Gateway de Pagamento / Fintech",
-          descricao: "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.",
-          proposta: 1300,
-          techs: [
-            "HTML",
-            "CSS",
-            "REACT"
-          ]
-        },
-        {
-          titulo: "Bot telegram Pagamento",
-          descricao: "Preciso fazer um código em python para um bot do telegram. O bot será para solicitação de pagamento.",
-          proposta: 2400,
-          techs: [
-            "PYTHON"
-          ]
-        },
-        {
-          titulo: "Caixa Rápido",
-          descricao: "Preciso fazer um software que permita ao usuário fazer o upload de seu extrato bancário em formato( ofx). Dentro do software o mesmo poderá categorizar todas as suas receitas e despesas, tendo categorias sugeridas pelo software e permitindo também personalizações. Após o lançamento de vários extratos o software irá entender que são lançamentos parecidos e fará a categorização de maneira automática, cabendo ao usuário somente categorizar as receitas e despesas que não se repetem. Após a categorização o software irá emitir gráficos e relatórios baseados na categorização das contas.",
-          proposta: 1200,
-          techs: [
-            "PYTHON"
-          ]
-        }
-      ]);
+  const [servicos, setServicos] = useState<any[]>([]);
+  const [skillDigitadaServicos, setSkillDigitadaServicos] = useState<string>("");
 
-      const [listaServicosFiltrados, setListaServicosFiltrados] = useState<any[]>(servicos);
-      const [skillDigitadaServicos, setSkillDigitadaServicos] = useState<string>("");
+  //funcao onde pega o que o usuario digitou
+  function verificarCampoSkillServicos(event: any) {
+    if (event.target.value === "") {
+      listarServicos();
+    } setSkillDigitadaServicos(event.target.value);
+  }
 
-      //funcao onde pega o que o usuario digitou
-      function verificarCampoSkillServicos(event: any) {
-        if (event.target.value === "") {
-          setListaServicosFiltrados(servicos);
-        } setSkillDigitadaServicos(event.target.value);
-      }
+  //   function buscarDevPorSkill(event: any) {
+  //     //não recarrega a pagina
+  //     event.preventDefault();
 
-    return (
-    
-            <>
-                <div id="sombra"></div>
-                
+  //     //filtrar devs pela skill digitada no campo buscar
+  //     const devsFiltrados = devs.filter((dev: any) => dev.hardSkills.includes(skillDigitado.toLocaleUpperCase()));
 
-                <main id="ListaServicosMain">
-                    <div className="container container_lista_servicos">
-                        <div className="lista_servicos_conteudo">
-                            <h1>Lista de Serviços</h1>
-                            <hr />
-                            <form method="post">
-                                <div className="wrapper_form">
-                                    <label htmlFor="busca">Procurar serviços</label>
-                                    <div className="campo-label">
-                                        <input type="search" name="campo-busca" id="busca" placeholder="Buscar serviços por tecnologias..." />
-                                        <button type="submit">Buscar</button>
-                                    </div>
-                                </div>
-                            </form>
-                            <div className="wrapper_lista">
-                                <ul>
-                                    { 
-                                    servicos.map((servico:any, indice:number) => {
-                                        return <li key={indice}>
-                                            <CardServico
-                                            titulo={servico.titulo}
-                                            descricao={servico.descricao}
-                                            proposta={servico.proposta}
-                                            listatechs={servico.techs}
+  //     if (devsFiltrados.length === 0) {
+  //         alert("Nenhum desenvolvedor(a) com essa skill :(")
+  //     } else {
+  //         //atribui valor de devs filtrado, ao state ListaDevsFiltrados 
+  //         setDevs(devsFiltrados);
+  //     }
 
-                                            />
-                                            </li>
-                                    })
+  // }
+
+  function buscarServicosPorSkill(event: any) {
+    event.preventDefault();
+
+    const servicosFiltrados = servicos.filter((servicos: any) => servicos.techs.includes(skillDigitadaServicos.toLocaleUpperCase()));
+
+    if (servicosFiltrados.length === 0) {
+      alert("Nenhum desenvolvedor(a) com essa skill :(")
+    } else {
+              //atribui valor de devs filtrado, ao state ListaDevsFiltrados 
+              setServicos(servicosFiltrados);
+          }
+  }
+
+  function listarServicos() {
+    api.get("servicos").then((response: any) => {
+      console.log(response);
+      setServicos(response.data)
+    })
+
+      .catch((error: any) => {
+        console.log("Erro ao realizar uma requisicao:"), error;
+      })
+  }
+
+  useEffect(() => {
+    //execute acao
+    listarServicos();
+  }, [])
 
 
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </main>
+  return (
+
+    <>
+      <div id="sombra"></div>
 
 
-            </>
-    
-    )
+      <main id="ListaServicosMain">
+        <div className="container container_lista_servicos">
+          <div className="lista_servicos_conteudo">
+            <h1>Lista de Serviços</h1>
+            <hr />
+            <form method="post" onSubmit={buscarServicosPorSkill}>
+              <div className="wrapper_form">
+                <label htmlFor="busca">Procurar serviços</label>
+                <div className="campo-label">
+                  <input type="search" name="campo-busca" id="busca" placeholder="Buscar serviços por tecnologias..." onChange={verificarCampoSkillServicos}/>
+                  <button type="submit">Buscar</button>
+                </div>
+              </div>
+            </form>
+            <div className="wrapper_lista">
+              <ul>
+                {
+                  servicos.map((servico: any, indice: number) => {
+                    return <li key={indice}>
+                      <CardServico
+                        titulo={servico.nome}
+                        descricao={servico.descricao}
+                        proposta={servico.valor}
+                        listatechs={servico.techs}
+
+                      />
+                    </li>
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+
+
+    </>
+
+  )
 }
 
 
